82b2774c0cce0f58ad501a782935f5dcc3ddba97
Muhammad Danish <danishjoki64@gmail.com>
2025-12-21 15:16:28 +0700

    update db

--- src/db_manager.py
+++ src/db_manager.py
@@ -5,15 +5,16 @@ import subprocess
 class DatabaseManager:
     def __init__(self, db_name='pyvault.db'):
         self.db_name = db_name
-        self.conn = sqlite3.connect(self.db_name)
+        self.conn = sqlite3.connect(self.db_name)  # 1. Koneksi ke SQLite
         self.cursor = self.conn.cursor()
-        self._create_tables()
-        self._harden_permission()
+        self._create_tables()                      # 2. Pastikan tabel tersedia
+        self._harden_permission()                  # 3. Kunci file database (Security)
     
     def _harden_permission(self):
         """Mengatur izin file ke 600 (Hanya owner Read/Write) di Linux."""
-        if os.name =='posix':
+        if os.name =='posix':   # Cek apakah OS-nya Linux/Unix
             try:
+                # Menjalankan perintah shell: chmod 600 pyvault.db
                 subprocess.run(['chmod', '600', self.db_name], check=True)
             except Exception as e:
                 print(f"[Warning] Gagal mengatur izin file DB")
@@ -27,7 +28,7 @@ class DatabaseManager:
             )
         ''')
         
-        """Tabel Vault (Data Password"""
+        """Tabel Vault (Data Password)"""
         self.cursor.execute('''
             CREATE TABLE IF NOT EXIST vault_items (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -37,3 +38,39 @@ class DatabaseManager:
                 nonce BLOB)
         ''')
         self.conn.commit()
+    
+    def is_initialized(self):
+        """Cek apakah tabel config ada isinya. Kalau 0 berarti aplikasi baru di install."""
+        self.cursor.execute("SELECT count(*) FROM security_config")
+        return self.cursor.fetchone()[0] > 0
+    
+    def save_config(self, salt, verifier):
+        """Simpan settingan keamanan saat Setup awal"""
+        self.cursor.execute("INSERT INTO security_config (salt, verifier) VALUES (?, ?)", (salt, verifier))
+        self.conn.commit()
+
+    def get_config(self):
+        """ Ambil salt & verifier untuk proses login"""
+        self.cursor.execute("SELECT salt, verifier FROM security_config")
+        return self.cursor.fetchone()
+    
+    def add_item(self, site, username, enc_data, nonce):
+        """ Simpan password baru yang sudah terenkripsi"""
+        self.cursor.execute(
+            "INSERT INTO vault_items (site_name, username, enc_data, nonce) VALUES (?, ?, ?, ?)",
+            (site, username, enc_data, nonce)
+        )
+        self.conn.commit()
+
+    def get_all_items(self):
+        """ Ambil daftar akun untuk ditampilkan di menu (Hanya info umum, bukan passwordnya """
+        self.cursor.execute("SELECT id, site_name, username FROM vault_items")
+        return self.cursor.fetchall()
+    
+    def get_item_by_id(self, item_id):
+        """ Ambil data terenkripsi spesifik untuk di dekripsi nanti"""
+        self.cursor.execute("SELECT enc_data, nonce FROM vault_items WHERE id = ?", (item_id,))
+        return self.cursor.fetchone()
+    
+    def close(self):
+        self.conn.close()
